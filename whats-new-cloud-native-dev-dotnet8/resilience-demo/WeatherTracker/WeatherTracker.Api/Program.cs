315b29c3f00c756609d6c5e70b7dc0caedf5d7d2
willvelida <willvelida@hotmail.co.uk>
2023-12-04 21:50:33 +1100

    Adding Resiliency Demo

---
+++ whats-new-cloud-native-dev-dotnet8/resilience-demo/WeatherTracker/WeatherTracker.Api/Program.cs
@@ -0,0 +1,45 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", async (CancellationToken cancellationToken) =>
+{
+    // Add these in to cause Mayhem!!!
+    //if (Random.Shared.NextDouble() < 0.2)
+    //{
+    //    await Task.Delay(5000, cancellationToken);
+    //}
+
+    //if (Random.Shared.NextDouble() < 0.3)
+    //{
+    //    throw new InvalidOperationException("Something went wrong");
+    //}
+
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+});
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ whats-new-cloud-native-dev-dotnet8/resilience-demo/WeatherTracker/WeatherTracker.Console/Program.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Http.Resilience;
+using Polly;
+using System.Diagnostics;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+IServiceCollection services = builder.Services;
+
+// Register HTTP client
+services.AddHttpClient("weather", client => client.BaseAddress = new Uri("http://localhost:5080"))
+	// Add your resilience handlers here!
+	.AddResilienceHandler("demo", builder =>
+	{
+		// 4. Rate limiter
+		builder.AddConcurrencyLimiter(100);
+
+		// 2. Retry pattern
+		builder.AddRetry(new HttpRetryStrategyOptions
+		{
+			MaxRetryAttempts = 5,
+			BackoffType = DelayBackoffType.Exponential,
+			UseJitter = true,
+			Delay = TimeSpan.Zero,
+		});
+
+		// 3. Circuit Breaker
+		builder.AddCircuitBreaker(new HttpCircuitBreakerStrategyOptions
+		{
+			SamplingDuration = TimeSpan.FromSeconds(5),
+			FailureRatio = 0.9,
+			MinimumThroughput = 5,
+			BreakDuration = TimeSpan.FromSeconds(5)
+		});
+
+		// 1 .Timeout
+		builder.AddTimeout(TimeSpan.FromSeconds(1));
+	});
+// Or, if you want these by default, just add:
+// // .AddStandardResilienceHandler()
+// // .Configure(options => { // use options to customize policies }
+
+
+// Create HTTP Client
+var httpClient = builder.Build().Services
+    .GetRequiredService<IHttpClientFactory>()
+    .CreateClient("weather");
+
+while(true)
+{
+	var watch = Stopwatch.StartNew();
+
+	try
+	{
+		using var response = await httpClient.GetAsync("weatherforecast");
+
+        Console.WriteLine($"{(int)response.StatusCode}: {watch.Elapsed.TotalMilliseconds,10:0.00}ms");
+    }
+	catch (Exception ex)
+	{
+        Console.WriteLine($"Err: {watch.Elapsed.TotalMilliseconds,10:0.00}ms ({ex.GetType().Name})");
+	}
+}
