39da64a02f87105ed4c26c78b7ce6c8e7ba7c2cd
willvelida <willvelida@hotmail.co.uk>
2023-12-05 10:36:42 +1100

    Adding metrics demo

---
+++ whats-new-cloud-native-dev-dotnet8/metrics-demo/WebMetric/CustomMetrics.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace WebMetric
+{
+    public class CustomMetrics
+    {
+        private readonly Counter<int> _productSoldCounter;
+
+        public CustomMetrics(IMeterFactory meterFactory)
+        {
+            var meter = meterFactory.Create("WebMetric.Shop");
+            _productSoldCounter = meter.CreateCounter<int>("web.metric.product.sold");
+        }
+
+        public void ProductSold(string productName, int quantity)
+        {
+            _productSoldCounter.Add(quantity, new KeyValuePair<string, object?>("web.metric.product.name", productName));
+        }
+    }
+}
+
+public class Product()
+{
+    public string ProductName { get; set; }
+    public int Quantity { get; set; }
+}
---
+++ whats-new-cloud-native-dev-dotnet8/metrics-demo/WebMetric/Program.cs
@@ -0,0 +1,37 @@
+using Bogus;
+using OpenTelemetry.Metrics;
+using WebMetric;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(builder =>
+    {
+        builder.AddPrometheusExporter();
+
+        builder.AddMeter("Microsoft.AspNetCore.Hosting",
+                         "Microsoft.AspNetCore.Server.Kestrel");
+        builder.AddView("http.server.request.duration",
+            new ExplicitBucketHistogramConfiguration
+            {
+                Boundaries = new double[] { 0, 0.005, 0.01, 0.025, 0.05,
+                       0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10 }
+            });
+    });
+builder.Services.AddSingleton<CustomMetrics>();
+var app = builder.Build();
+
+app.MapPrometheusScrapingEndpoint();
+
+app.MapGet("/", () => "Hello OpenTelemetry! ticks:"
+                     + DateTime.Now.Ticks.ToString()[^3..]);
+
+app.MapGet("/sale", (CustomMetrics metrics) =>
+{
+    var fakeProduct = new Faker<Product>()
+    .RuleFor(p => p.ProductName, (f) => f.Commerce.ProductName())
+    .RuleFor(p => p.Quantity, (f) => f.Random.Number(1, 5))
+    .Generate();
+    metrics.ProductSold(fakeProduct.ProductName, fakeProduct.Quantity);
+});
+
+app.Run();
