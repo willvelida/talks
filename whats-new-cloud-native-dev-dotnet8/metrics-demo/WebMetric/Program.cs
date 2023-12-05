9140558ff2ef0ea17d98c8853bdb936049055e12
willvelida <willvelida@hotmail.co.uk>
2023-12-05 13:53:17 +1100

    Updating slides, adding demo notes

--- whats-new-cloud-native-dev-dotnet8/metrics-demo/WebMetric/Program.cs
+++ whats-new-cloud-native-dev-dotnet8/metrics-demo/WebMetric/Program.cs
@@ -17,6 +17,8 @@ builder.Services.AddOpenTelemetry()
                        0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10 }
             });
     });
+
+// Register the metrics type using DI
 builder.Services.AddSingleton<CustomMetrics>();
 var app = builder.Build();
 
@@ -25,6 +27,7 @@ app.MapPrometheusScrapingEndpoint();
 app.MapGet("/", () => "Hello OpenTelemetry! ticks:"
                      + DateTime.Now.Ticks.ToString()[^3..]);
 
+// We can now inject the custom metrics type and record values where needed.
 app.MapGet("/sale", (CustomMetrics metrics) =>
 {
     var fakeProduct = new Faker<Product>()
